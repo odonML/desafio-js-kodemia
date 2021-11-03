31eed88e65270435082c7d73172afd8333744426
Ftnt <jairo.rocano@gmail.com>
2021-11-03 06:49:28 -0500

    feature(general):se modifico la estructura de boostrap a scss html y js puro

---
+++ js/desingHome.js
@@ -0,0 +1,19 @@
+
+
+document.querySelector(".cards-secondary").addEventListener("click", function (event) {
+    let card = document.querySelector(".card")
+    addClass(card, "bordCard")
+});
+
+function addClass(el, className) {
+    if (el.classList)
+        el.classList.add(className)
+    else if (!hasClass(el, className))
+        el.className += " " + className;
+}
+
+function hasClass(el, className) {
+    if (el.classList)
+        return el.classList.contains(className);
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
